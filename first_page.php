bdae51378aee73e1373aa0203e72792756fab92b
NashS <nasseq@gmail.com>
2013-06-09 14:39:02 +0000

    msg

---
+++ first_page.php
@@ -0,0 +1,10 @@
+<?php
+echo "My first PHP script!";
+?>
+
+<!DOCTYPE html>
+<html>
+<body>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+  print <<< EOT
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Test successful</title>
+</head>
+<body>
+<h1>Test successful</h1>
+<p>Congratulations.</p>
+<p>Apache and PHP are working.Derp a derp</p>
+<p>Here is a link: <a href="first_page.php">click it</a></p>
+</body>
+</html>
+EOT;
+
+?>
